b1f1e09dee52004e305014536a2a381cc2491a74
shcox <45539562+shcox@users.noreply.github.com>
2021-10-25 20:03:07 -0400

    Added a GET, fixed the POSTs

--- users/users.js
+++ users/users.js
@@ -5,8 +5,9 @@ const cors = require('cors')
 const path = require('path')
 
 const app = express()
-const port = process.env.USER_PORT || 4003
+const port = process.env.REVIEW_PORT || 4003
 
+app.use(express.urlencoded())
 app.use(cors())
 app.use(express.json())
 
@@ -23,42 +24,80 @@ let users = {
     }
 }
 
-app.post('api/users/favorite_item', (req, res) => {
-    if (users.hasOwnProperty(req.body.username)) {
-        let favorites = users[req.body.username].favorites;
-        // for (let i = 0; i < favorites.length; i++) {
-        //     if (req.body.item_id === favorites[i]) {
-        //         res.status(404).send(users);
-        //     }
-        // }
-        if (favorites.includes(req.body.item_id)) {
-            users[req.body.username].favorites.push(req.body.item_id);
-            res.status(200).send(users);
-        } else {
-            res.status(404).end();
-        }
+app.get('/api/users/favorites', (req, res) => {
+    if (users.hasOwnProperty(req.query.username)) {
+        res.status(200).send(users[req.query.username]);
     } else {
         res.status(404).end();
     }
 })
 
-app.post('api/users/unfavorite_item', (req, res) => {
-    if(users.hasOwnProperty(req.body.username)) {
-        let favorites = users[req.body.username].favorites;
-        if (!(favorites.includes(req.body.item_id))) {
-            res.status(404).end();
-        } else {
-            for (let i = 0; i < favorites.length; i++) {
-                if (req.body.item_id === favorites[i]) {
-                    favorites.splice(i, 1);
-                    res.status(200).send(users);
-                }
-            }
-        }
+app.post('/api/users/favorite_item', (req, res) => {
+    console.log(req.body);
+    if (req.body.username === undefined || req.body.item_id === undefined) {
+        res.status(400).end()
     } else {
-        res.status(404).end();
+        if (users.hasOwnProperty(req.body.username)) {
+            // console.log("In users.");
+            let favorites = users[req.body.username].favorites;
+            console.log(favorites);
+            // for (let i = 0; i < favorites.length; i++) {
+            //     if (req.body.item_id === favorites[i]) {
+            //         res.status(404).send(users);
+            //     }
+            // }
+            if (!favorites.includes(req.body.item_id)) {
+                users[req.body.username].favorites.push(req.body.item_id);
+                res.status(201).send(users);
+            } else {
+                // maybe change this 
+                res.status(404).end();
+            }
+        } else {
+            res.status(404).end();
+        }
     }
 })
+
+app.post('/api/users/unfavorite_item', (req, res) => {
+    if (req.body.username === undefined || req.body.item_id === undefined) {
+        res.status(400).end();
+    } else {
+        if(users.hasOwnProperty(req.body.username)) {
+            let favorites = users[req.body.username].favorites;
+            if (!(favorites.includes(req.body.item_id))) {
+                res.status(404).end();
+            } else {
+                for (let i = 0; i < favorites.length; i++) {
+                    if (req.body.item_id === favorites[i]) {
+                        favorites.splice(i, 1);
+                        res.status(200).send(users);
+                    }
+                }
+            }
+        } else {
+            res.status(404).end();
+        }
+    }
+})
+
+if (process.platform === "win32") {
+    var rl = require("readline").createInterface({
+      input: process.stdin,
+      output: process.stdout
+    });
+  
+    rl.on("SIGINT", function () {
+      process.emit("SIGINT");
+    });
+  }
+  
+  process.on("SIGINT", async function () {
+    // graceful shutdown
+    //await axios.delete('http://localhost:2525/imposters/4545');
+    //await axios.delete('http://localhost:2525/imposters/5555');
+    process.exit();
+  });
 
 app.listen(port, () => {
     console.log(`server listening on the port::${port}`)
