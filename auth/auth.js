a9344bacd80f428cf88fc7fa60f8e271e2fdd74f
YASMEEN <yasmeenmekky@gmail.com>
2021-11-15 17:28:55 -0500

    integrate mysql

--- auth/auth.js
+++ auth/auth.js
@@ -2,7 +2,7 @@
 require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
-const path = require('path')
+const validEmail = require('email-format-check')
 
 const app = express()
 const port = process.env.AUTH_PORT || 4000
@@ -11,62 +11,79 @@ const port = process.env.AUTH_PORT || 4000
 app.use(cors())
 app.use(express.json())
 
-const users = {
-    "johndoe" : {password: "123456", sessionId: null},
-    "person" : {password: "abcde", sessionId: null}
-}
+const mysql = require('mysql')
+const e = require('express')
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'galactic',
+  database: 'auth'
+})
+
+connection.connect((err) => {
+    if(err){
+      console.log('Error connecting to DB')
+      return
+    }
+    console.log('DB Connection established')
+})
+
 
 app.post('/api/auth/sign_up', async (req, res) => {
-    const username = req.body.user
-    const password = req.body.password
+    const email = req.body.email
+    const pass = req.body.pass
+    
+    if(email === undefined || pass === undefined) res.status(400).end()
+    else if(!validEmail(email)) res.status(400).send({message: 'invalid-email'})
+    else if(pass === '') res.status(400).send({message: 'invalid-password'})
+    else {
+        const queryUserExists = `SELECT * FROM users WHERE email = "${email}"`
+        
+        connection.query(queryUserExists, (err, rows) => {
+            if(err) res.status(500).send({message: 'error-processing-request'}) //sql error
+            else if(rows && rows.length > 0) res.status(403).send({message: 'user-already-exists'})
+            else {
+                const insertUser = `INSERT INTO users(id, email, pass) VALUES(NULL, '${email}', '${pass}')`
 
-    if(username === undefined || password === undefined) res.status(400).end()
-
-    if(users.hasOwnProperty(username)){
-        res.status(403).send({message: 'user-already-exists'})
-    }
-
-    //skeleton for future async functionality 
-    try {
-        users[username] = {
-            password, 
-            sessionId: null
-        }
-        res.status(201).send({message: 'user-created'})
-    }
-    catch(err) {
-        res.status(500).send({message: 'error-processing-request'})
+                connection.query(insertUser, (err, rows) => {
+                    if(err) res.status(500).send({message: 'sql-error'})    
+                    else res.status(201).send({message: 'user-created'})
+                })
+            }
+        })
     }
 })
 
 app.get('/api/auth/log_in', (req, res) => {
-    const username = req.query.user
-    const password = req.query.password
+    const email = req.query.email
+    const pass = req.query.pass
 
-    if(username === undefined || password === undefined) res.status(400).end()
-
-    if(!users.hasOwnProperty(username)) {
-        res.status(404).send({message: 'user-not-found'})
-    }
-    else if(password === users[username].password) {
-        users[username].sessionId = Math.random()
-        res.status(200).send({message: 'login-successful'})
-    }
+    if(email === undefined || pass === undefined) res.status(400).end()
     else {
-        res.status(403).send({message: 'incorrect-password'})
+        const queryUser = `SELECT * FROM users WHERE email = "${email}"`
+        
+        connection.query(queryUser, (err, rows) => {
+            if(err) res.status(500).send({message: 'error-processing-request'}) //sql error
+            else if(!rows || rows.length === 0 ) res.status(404).send({message: 'user-not-found'})
+            else if(rows.length > 1) res.status(500).send({message: 'duplicate-users'})
+            else { 
+                if(pass === rows[0]["pass"] )res.status(200).send({message: 'login-successful'})
+                else res.status(403).send({message: 'incorrect-password'})
+            }
+        })
     }
 })
 
 app.get('/api/auth/log_out', (req, res) => {
-    const username = req.query.user
+    const email = req.query.user
 
-    if(username === undefined) res.status(400).end()
+    if(email === undefined) res.status(400).end()
 
-    if(!users.hasOwnProperty(username)) {
+    if(!users.hasOwnProperty(email)) {
         res.status(404).send({message: 'user-not-found'})
     }
     else {
-        users[username].sessionId = null
+        users[email].sessionId = null
         res.status(200).send({message: 'logged-out'})
     }
 })
@@ -74,3 +91,12 @@ app.get('/api/auth/log_out', (req, res) => {
 app.listen(port, () => {
     console.log(`server listening on the port::${port}`)
 })
+
+//gives me an err
+/* 
+connection.end((err) => {
+    // The connection is terminated gracefully
+    // Ensures all remaining queries are executed
+    // Then sends a quit packet to the MySQL server.
+})*/
+  
